1962230a01a094ba92d9fae513fc8f8a069afbe2
febrian syahroni <febrian.fs716@gmail.com>
2025-01-15 03:54:29 +0700

    setup provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { AuthProvider } from "./contexts/auth.tsx";
+import { Provider } from "react-redux";
 import App from "./App.tsx";
+import { store } from "./store";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </AuthProvider>
   </StrictMode>
 );
